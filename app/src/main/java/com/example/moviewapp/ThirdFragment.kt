4946c92dc3a2d3ecca2a84d92a845b23a458bba9
lrbl <luis.lemini.98@gmail.com>
2022-04-22 17:55:27 -0500

    Video

--- app/src/main/java/com/example/moviewapp/ThirdFragment.kt
+++ app/src/main/java/com/example/moviewapp/ThirdFragment.kt
@@ -1,22 +1,24 @@
 package com.example.moviewapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
+import com.example.moviewapp.databinding.FragmentSecondBinding
+import com.example.moviewapp.databinding.FragmentThirdBinding
 
 class ThirdFragment : Fragment() {
 
+    private var _binding : FragmentThirdBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val movieSelected = requireArguments().getString("title")
-        Toast.makeText(
-            activity, movieSelected,
-            Toast.LENGTH_LONG
-        ).show()
     }
 
     override fun onCreateView(
@@ -24,6 +26,32 @@ class ThirdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        val movie = requireArguments().getString("title")
+        var movieSelected = ""
+        if (movie == "Wall-E") {
+            movieSelected = "walle"
+        }  else if (movie == "Ratatouille") {
+            movieSelected = "rata"
+        }
+        else if (movie == "Up") {
+            movieSelected = "up"
+        }
+        else if (movie == "Coco") {
+            movieSelected = "coco"
+        }
+        else if (movie == "Aladdin") {
+            movieSelected = "ala"
+        }
+        _binding = FragmentThirdBinding.inflate(inflater, container,false)
+        val root: View = binding.root
+        val videoViewer = binding.videoView
+        val videoPath = "android.resource://" + requireActivity().packageName +"/raw/${movieSelected}"
+        val uri = Uri.parse(videoPath)
+        videoViewer.setVideoURI(uri)
+        val mediaController: MediaController = MediaController(root.context)
+        mediaController.setAnchorView(videoViewer)
+        videoViewer.setMediaController(mediaController)
+        videoViewer.start()
+        return root
     }
 }
